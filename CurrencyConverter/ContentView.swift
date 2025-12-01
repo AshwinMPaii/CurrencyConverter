3f14be059e8c36a2e382e85963b7a5f7732be75a
Ashwin pai <ashwinpai808@gmail.com>
2025-12-01 11:05:05 +0530

    Initial Commit

---
+++ CurrencyConverter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CurrencyConverter
+//
+//  Created by Ashwin Pai on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding()
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Ashwin Pai on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
